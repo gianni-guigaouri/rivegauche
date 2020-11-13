bae34ca0c1c09ab5fcd0ecc5a37bc414dfa9b619
gianni-guigaouri <giannimarchello92@gmail.com>
2020-11-13 02:06:53 +0100

    added data in redux folder and create reducer for each file

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,24 @@
 import React from 'react'
-import SHOP_DATA from './shop.data'
+
+import { connect } from 'react-redux'
+import { selectShopCollections } from '../../redux/shop/shop.selectors'
+import { createStructuredSelector } from 'reselect'
 
 import CollectionPreview from '../../components/preview-collection/collection-preview.component'
 
-class ShopPage extends React.Component {
-  constructor (props) {
-    super(props)
+const ShopPage = ({ collections }) => (
 
-    this.state = {
-      collections: SHOP_DATA
+  <div className='shop-page'>
+    {
+      collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))
     }
-  }
+  </div>
+)
 
-  render () {
-    const { collections } = this.state
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-    )
-  }
-}
+const mapDispatchToProps = createStructuredSelector({
+  collections: selectShopCollections
+})
 
-export default ShopPage
+export default connect(mapDispatchToProps)(ShopPage)
